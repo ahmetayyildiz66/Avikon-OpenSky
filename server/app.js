34e69248c2c7ef19df49bd23712ff009efd0abaa
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2019-07-28 13:50:28 +0300

    Backend completed

--- server/app.js
+++ server/app.js
@@ -14,39 +14,14 @@ const server = app.listen(3000, () => {
 
 var io = require('socket.io')(server);
 
-// var requestLoop = setInterval(function() {
-//   api_call
-//     .API_Call(url)
-//     .then(response => {
-//       var result = parsing.parseFunc(response.states);
-//       io.on('connection', function(socket){
-//         console.log(socket.id)
-//         socket.on('SEND_MESSAGE', function(data){
-//           io.emit('MESSAGE', data)
-//         });
-//         socket.disconnect(true);
-//       });
-
-//       //res.json(result);
-//     })
-//     .catch(error => {
-//       //res.send(error);
-//     });
-// }, 15000);
 
 io.on('connection', (socket) =>{
-  console.log(socket.id)
-  var counter = 0;
   var requestLoop = setInterval(function(){
-    counter++;
     api_call.API_Call(url)
       .then(response => {
         var result = parsing.parseFunc(response.states);
-        //console.log('result: '+counter + " "+result);
         socket.emit('RESPONSE',result);
-        // socket.on('SEND_MESSAGE', function(data){
-        //   io.emit('MESSAGE', data)
-        // });
+        
       })
       .catch(error => {
 
--- server/parseFunc.js
+++ server/parseFunc.js
@@ -1,7 +1,7 @@
 exports.parseFunc = function parse(data){
     var results = []
     for(var i = 0; i<data.length; i++){
-        results.push([data[i][0],data[i][1],data[i][2],data[i][5],data[i][6],data[i][8],data[i][9]])
+        results.push([data[i][1],data[i][2],data[i][5],data[i][6],data[i][8],data[i][9]])
     }
     return results;
 }
